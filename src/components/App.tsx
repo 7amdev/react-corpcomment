ceaa393e73d66cd51df040c5e2c55fd3e6a75f70
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-01-30 11:40:00 -0100

    add header styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,19 +6,32 @@ export default function App() {
       </footer>
       <main>
         <header className="header">
-          <p className="logo">
-            Corp<span className="logo__comment">Comment</span>
-          </p>
+          <img
+            className="header__background"
+            src="https://bytegrad.com/course-assets/js/1/pattern.svg"
+            alt=""
+          />
+          <a href="/" className="logo">
+            <img
+              className="logo__img"
+              src="https://bytegrad.com/course-assets/js/1/logo.svg"
+              alt="logo image"
+            />
+          </a>
           <h1 className="title">
             Give Feedback. <span className="title__publicly">Publicly.</span>
           </h1>
-          <form className="feedback-form" action="#">
+          <form className="feedback-form feedback-form_invalid " action="#">
             <textarea
-              id="feedback"
+              id="feedback-input"
               className="feedback-form__textarea"
               spellCheck="false"
-            ></textarea>
-            <label htmlFor="feedback" className="feedback-form__placeholder">
+              placeholder="."
+            />
+            <label
+              htmlFor="feedback-input"
+              className="feedback-form__placeholder"
+            >
               Enter your feedback here, remenber to #hashtag the company
             </label>
             <span className="feedback-form__count">150</span>
