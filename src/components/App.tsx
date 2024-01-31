a40b82c251a0f23be4b7b05be6789ec1af396d18
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-01-31 08:23:42 -0100

    style list-item on focus and hover events

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -43,7 +43,14 @@ export default function App() {
         <ol className="feedbacks">
           <li className="feedback">
             <button className="feedback__button">
-              <svg className="feedback__icon">
+              <svg
+                className="feedback__icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M4 9H11L7.5 4.5L4 9Z" fill="currentColor"></path>
               </svg>
               593
@@ -61,7 +68,14 @@ export default function App() {
           </li>
           <li className="feedback">
             <button className="feedback__button">
-              <svg className="feedback__icon">
+              <svg
+                className="feedback__icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M4 9H11L7.5 4.5L4 9Z" fill="currentColor"></path>
               </svg>
               298
